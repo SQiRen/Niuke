8a9b4b23fd76ecd73ac9c22adcf9f23d17abc5ac
rsq13500961288 <1434419753@qq.com>
2020-03-18 16:23:36 +0800

    添加项目ApproximateNumber

---
+++ Niuke/ApproximateNumber.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+	int n, number;
+	while (scanf("%d\n", &n) != EOF) {
+		if (n == 0  || n > 1000)
+			break;
+		for (int i = 0; i < n; i++) {
+			scanf("%d ", &number);
+			int flag = 0,j;
+			for (j = 1; j * j <= number; j++) {
+				
+				if (number % j == 0)
+					flag+=2;
+
+			}
+			if (j * j == number)
+				flag++;
+			printf("%d\n", flag);
+		}
+
+	}
+	return 0;
+}
