f77137fdf31da74dfaa9359a071e13ae6f5b8842
rsq13500961288 <1434419753@qq.com>
2020-03-18 16:56:38 +0800

    提交项目ProxyServer

--- Niuke/ApproximateNumber.cpp
+++ Niuke/ApproximateNumber.cpp
@@ -6,15 +6,15 @@ using namespace std;
 int main() {
 	int n, number;
 	while (scanf("%d\n", &n) != EOF) {
-		if (n == 0  || n > 1000)
+		if (n == 0 || n > 1000)
 			break;
 		for (int i = 0; i < n; i++) {
 			scanf("%d ", &number);
-			int flag = 0,j;
+			int flag = 0, j;
 			for (j = 1; j * j <= number; j++) {
-				
+
 				if (number % j == 0)
-					flag+=2;
+					flag += 2;
 
 			}
 			if (j * j == number)
